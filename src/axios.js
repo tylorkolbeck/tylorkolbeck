8065d58114b95d5dc938a0a804e52c36c512727f
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2018-11-29 23:58:37 -0800

    Removed something

--- src/axios.js
+++ src/axios.js
@@ -12,13 +12,7 @@ const instance = axios.create({
 
 })
 
-// instance.defaults.headers.common['Authorization'] = 'AIzaSyDujaaPXAWWkkPVl773HB6mUHEZFd7a8no'
-// instance.defaults.headers.get['Accepts'] = 'application/json'
-// instance.defaults.headers.common['authDomain'] = 'my-website-8af43.firebaseapp.com'
-// instance.defaults.headers.common['databaseURL'] = 'https://my-website-8af43.firebaseio.com'
-// instance.defaults.headers.common['projectId'] = 'my-website-8af43'
-// instance.defaults.headers.common['storageBucket'] = 'my-website-8af43.appspot.com'
-// instance.defaults.headers.common['messagingSenderId'] = '308390776707'
+
 
 
 
