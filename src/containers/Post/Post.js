e32022c65f81a412d4c79d63b41cdc2a99e4b014
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2018-12-04 13:58:41 -0800

    Added delete post functionality

--- src/containers/Post/Post.js
+++ src/containers/Post/Post.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import { Link, NavLink } from 'react-router-dom'
 import './Post.css'
+import {deletePostHandler} from './deletePost'
 
 
 const post = props => {
@@ -11,7 +12,6 @@ const post = props => {
       
       arr.forEach((tag)=> {
         if (tag.length > 0) {
-          // tagArray.push(<Link to={filterRoute} key={tag}><div className='tag_block' key={tag} onClick={props.tagFilter}>{tag}</div></Link>)
           tagArray.push(<div className='tag_block' key={tag + arrayCounter} onClick={props.tagFilter}>{tag}</div>)
         }
         arrayCounter++
@@ -20,22 +20,18 @@ const post = props => {
     } 
     return null
   }
-
-
   
   const showDeleteButton = () => {
     let deleteButton = null
     let isAdmin = props.userId ? true : false
     if (isAdmin) {
-      deleteButton = <span>x</span>
-    } else {
-      console.log('Not admin')
-    }
+      deleteButton = <span id={props.id} onClick={() => deletePostHandler(props.id, props.userId)} className="post__delete_post_button">x</span>
+    } 
     return deleteButton
   }
   
- 
   return (
+      
       <div>
         <div className='Sync'>
         <div className='sync__date__desktop Courier'>
@@ -45,7 +41,7 @@ const post = props => {
         </div>
     
         <div className='sync__main__info'>
-            <h1 onClick={props.clicked}>{props.title}{showDeleteButton()}</h1>
+            <h1 style={{display: 'block', width: '100%'}}>{props.title}<span>{showDeleteButton()}</span></h1>
             <h3><span className='bold' style={{fontSize: '.9rem', paddingLeft: '16px'}}>Author: </span><span style={{fontSize: '.9rem'}}>{props.author} </span> <span className='sync__date__mobile'>{props.date[2] + props.date[1].toUpperCase() + props.date[0].toString().slice(-2)}</span></h3>
             <p style={{paddingLeft: '16px'}}>{props.description}</p>
             <div className='tags' style={{paddingLeft: '16px'}}><span>{tagLoop(props.tags)}</span></div>
