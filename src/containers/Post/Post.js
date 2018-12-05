4b7c42c1bebbe6b3d533d722fc92ef3a2f9b762a
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2018-12-04 21:05:25 -0800

    small bug fix when deleteing posts

--- src/containers/Post/Post.js
+++ src/containers/Post/Post.js
@@ -12,7 +12,6 @@ class post extends Component {
     this.setState({isDeleted: true})
   }
 
-  // deletePostHandler = this.deletePostHandler.bind(this)
 
   showDeleteButton() {
     let deleteButton = null
@@ -82,55 +81,6 @@ render () {
     )  
   
 }
-
-
-// const post = props => {
-//   const tagLoop = (arr) => {
-//     const tagArray = []
-//     if (Array.isArray(arr) && arr.length > 0) {
-//       let arrayCounter = 0;
-      
-//       arr.forEach((tag)=> {
-//         if (tag.length > 0) {
-//           tagArray.push(<div className='tag_block' key={tag + arrayCounter} onClick={props.tagFilter}>{tag}</div>)
-//         }
-//         arrayCounter++
-//       })
-//       return tagArray
-//     } 
-//     return null
-//   }
-  
-//   const showDeleteButton = () => {
-//     let deleteButton = null
-//     let isAdmin = props.userId ? true : false
-//     if (isAdmin) {
-//       deleteButton = <span id={props.id} onClick={() => deletePostHandler(props.id, props.userId)} className="post__delete_post_button">x</span>
-//     } 
-//     return deleteButton
-//   }
-  
-//   return (
-      
-//       <div>
-//         <div className='Sync'>
-//         <div className='sync__date__desktop Courier'>
-//             <p className='month'>{props.date[1]}</p>
-//             <p className='date'>{props.date[2]}</p>
-//             <p className='year'>{props.date[0]}</p>
-//         </div>
-    
-//         <div className='sync__main__info'>
-//             <h1 style={{display: 'block', width: '100%'}}>{props.title}<span>{showDeleteButton()}</span></h1>
-//             <h3><span className='bold' style={{fontSize: '.9rem', paddingLeft: '16px'}}>Author: </span><span style={{fontSize: '.9rem'}}>{props.author} </span> <span className='sync__date__mobile'>{props.date[2] + props.date[1].toUpperCase() + props.date[0].toString().slice(-2)}</span></h3>
-//             <p style={{paddingLeft: '16px'}}>{props.description}</p>
-//             <div className='tags' style={{paddingLeft: '16px'}}><span>{tagLoop(props.tags)}</span></div>
-//         </div>
-
-//         </div>
-//     </div>
-//   )
-// }
 }
 
 export default post
--- src/containers/Post/deletePost.js
+++ src/containers/Post/deletePost.js
@@ -8,17 +8,14 @@ export const deletePostHandler = (postId, userId, stateHandler) => {
         }
         const deleteUrl = 'http://localhost:3000/posts/' + postId
         const axiosConfigObject = {headers: httpReqHeaders}
-        // stateHandler()
         axios.delete(deleteUrl, axiosConfigObject)
-        // axios.delete('/posts/' + postId) 
         .then((res) => {
             console.log(res)
-            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE', 'PATCH' );
+            stateHandler()
         })
-        .then(stateHandler()) // Visually remove the item from the screen
+        // Visually remove the item from the screen
         .catch(err => console.log(err))
-        console.log("POST ID: ", postId)
-        console.log("USER ID: ", userId)
+
     }
 
     
