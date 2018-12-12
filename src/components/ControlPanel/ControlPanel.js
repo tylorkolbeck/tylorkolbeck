b312ebcb946dcbd4f38693fbe227b86ef495cf67
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2018-12-11 17:56:40 -0800

    added icons to the control panel

--- src/components/ControlPanel/ControlPanel.js
+++ src/components/ControlPanel/ControlPanel.js
@@ -6,9 +6,9 @@ const ControlPanel = props => {
         <div className="ControlPanel__slideout">
             <div className="ControlPanel__slideout_inner">
                 <div>
-                    <a href="/edit-post">New |</a>
-                    <a href="/contactMsgs">Messages | </a>
-                    <a href="/contactMsgs">Admin </a>
+                    <a href="/edit-post"> <i className="material-icons">note_add</i></a>
+                    <a href="/contactMsgs"><i className="material-icons">question_answer</i> </a>
+                    <a href="/contactMsgs"><i className="material-icons">lock</i></a>
                 </div>
             </div>
         </div>
