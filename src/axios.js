9d1eb018b2a130db4b6c350b23781a0f5184b337
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2018-12-24 15:31:05 -0800

    Added HTTPS

--- src/axios.js
+++ src/axios.js
@@ -3,13 +3,13 @@ import https from 'https'
 
 // Set up for axios
 // axios.defaults.baseURL = 'http://localhost:3000'
-axios.defaults.baseURL = 'https://54.184.193.9:3000'
+axios.defaults.baseURL = 'http://54.184.193.9:3000'
 
 
 
 const instance = axios.create({
   // baseURL: 'http://localhost:3000',
-  baseURL: 'https://54.184.193.9:3000',
+  baseURL: 'http://54.184.193.9:3000',
   httpsAgent: new https.Agent({  
     rejectUnauthorized: false
   })
