b6bfd9b6ea00d7dd570d9a1ba567fafc5e0acad1
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2018-12-25 10:18:17 -0800

    test

--- src/axios.js
+++ src/axios.js
@@ -2,14 +2,15 @@ import axios from 'axios'
 import https from 'https'
 
 // Set up for axios
-// axios.defaults.baseURL = 'http://localhost:3000'
-axios.defaults.baseURL = 'http://54.184.193.9:3000'
+// axios.defaults.baseURL = 'http://localhost:3000'  // For local development testing
+axios.defaults.baseURL = 'http://54.148.26.236:3000' // For deployed state
 
 
 
 const instance = axios.create({
-  // baseURL: 'http://localhost:3000',
-  baseURL: 'http://54.184.193.9:3000',
+  // baseURL: 'http://localhost:3000',  //  For local development testing
+  baseURL: 'http://54.148.26.236:3000', //  For deployed state
+  
   httpsAgent: new https.Agent({  
     rejectUnauthorized: false
   })
