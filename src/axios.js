ae1e096823c427e83eecba015b00bde43412f7b9
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2019-01-04 20:23:36 -0800

    trying to get https working

--- src/axios.js
+++ src/axios.js
@@ -3,13 +3,13 @@ import https from 'https'
 
 // Set up for axios
 // axios.defaults.baseURL = 'http://localhost:3000'  // For local development testing
-axios.defaults.baseURL = 'http://54.148.26.236:443' // For deployed state
+axios.defaults.baseURL = 'https://54.148.26.236:443' // For deployed state
 
 
 
 const instance = axios.create({
   // baseURL: 'http://localhost:3000',  //  For local development testing
-  baseURL: 'https://54.148.26.236:3000', //  For deployed state
+  baseURL: 'https://54.148.26.236:443', //  For deployed state
   
   httpsAgent: new https.Agent({  
     rejectUnauthorized: false
