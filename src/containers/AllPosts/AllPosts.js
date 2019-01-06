b104f527ea1729cb6e0339394af897f2402c38f3
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2019-01-06 12:36:17 -0800

    trying to get https set up

--- src/containers/AllPosts/AllPosts.js
+++ src/containers/AllPosts/AllPosts.js
@@ -132,7 +132,7 @@ class AllPosts extends Component {
     }
     else if (this.state.filter.length === 0) {
       // axios.get('/posts')
-      axios.get('https://54.148.26.236:443/posts')
+      axios.get('https://rest-blog.thedailyfunc.com/posts')
       .then((response => {
         let posts = []   
   
